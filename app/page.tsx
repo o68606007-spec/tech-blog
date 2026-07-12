8b7ff10def7e579c92f130973cfa6931fca71b34
o68606007-spec <test@DESKTOP-SRSUEFO.localdomain>
2026-07-12 20:18:55 +0900

    deploy commit 9th

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
     return (
         <>
             <div className="space-y-12">
-                {/* Hero */}
+                {/* Hero1 */}
                 <Hero />
 
                 {/* Qiita Section */}
